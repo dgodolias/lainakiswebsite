3fdf2458f29b45d9859c77ce0b858073c3ccc064
dgodolias <dgodolias@gmail.com>
2024-11-05 17:20:31 +0200

    more changes on fonts, carousel doesnt work ok

--- js/resizing.js
+++ js/resizing.js
@@ -35,9 +35,13 @@ function adjustFontSize(config) {
 // Configuration for different elements and their respective percentages, exponents, resizing points, freezeBelow flag, CSS property, and dependent dimension
 const fontSizeConfig = [
     { selector: '#top-left-name', percentage: 0.015, exponent: 0.9, cssProperty: 'fontSize', dependentOn: 'height' },
-    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.01, exponent: 0.9, cssProperty: 'fontSize', dependentOn: 'height' },
-    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.006, exponent: 0.9, cssProperty: 'margin-left', dependentOn: 'width' },
-    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.006, exponent: 0.9, cssProperty: 'margin-right', dependentOn: 'width' },
+    { selector: '#top-left-name', percentage: 0.006, exponent: 1, cssProperty: 'margin', dependentOn: 'height' },
+    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.01, exponent: 1, cssProperty: 'fontSize', dependentOn: 'height' },
+    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.006, exponent: 1, cssProperty: 'margin-left', dependentOn: 'width' },
+    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.006, exponent: 1, cssProperty: 'margin-right', dependentOn: 'width' },
+    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.012, exponent: 1, cssProperty: 'margin-top', dependentOn: 'height' },
+    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.012, exponent: 1, cssProperty: 'margin-bottom', dependentOn: 'height' },
+    { selector: '#menu.navbar-default .navbar-nav>li>a', percentage: 0.0045, exponent: 1, cssProperty: 'padding', dependentOn: 'height' },
     { selector: '#header-title-inside h1', percentage: 0.023, exponent: 0.9, resizingPoints: [[1200, 0.05]], freezeBelow: true, cssProperty: 'fontSize', dependentOn: 'width' },
     { selector: '#header', percentage: 0.7, exponent: 0.9, freezeBelow: true, cssProperty: 'height', dependentOn: 'height' },
     { selector: '.coloured-faded-text', percentage: 0.02, exponent: 0.9, cssProperty: 'fontSize', dependentOn: 'height' },
@@ -45,10 +49,14 @@ const fontSizeConfig = [
     { selector: '#profile-pic', percentage: 0.25, exponent: 0.9, resizingPoints: [[1200, 0.348]], freezeBelow: true, cssProperty: 'width', dependentOn: 'width' },
     { selector: '#profile-pic', percentage: 0.3, exponent: 0.9, resizingPoints: [[1200, 0.426]], freezeBelow: true, cssProperty: 'height', dependentOn: 'height' },
     { selector: '.services-text h2', percentage: 0.006, exponent: 0.9, resizingPoints: [[1640, 0.03]], freezeBelow: true, cssProperty: 'fontSize', dependentOn: 'width' },
+    { selector: '#services', percentage: 0.22, exponent: 1, cssProperty: 'padding-top', dependentOn: 'height' },
+    { selector: '#services', percentage: 0.22, exponent: 1, cssProperty: 'padding-bottom', dependentOn: 'height' },
     { selector: '.services-text li', percentage: 0.01, exponent: 0.9, cssProperty: 'fontSize', dependentOn: 'height' },
     { selector: '#contact-container-specific', percentage: 0.35, exponent: 0.9, resizingPoints: [[900, 0.4]], freezeBelow: true, cssProperty: 'height', dependentOn: 'height' },
     { selector: '.screen-header-button', percentage: 0.007, exponent: 0.9, resizingPoints: [[900, 0.02]], freezeBelow: true, cssProperty: 'width', dependentOn: 'width' },
     { selector: '.screen-header-button', percentage: 0.007, exponent: 0.9, resizingPoints: [[900, 0.02]], freezeBelow: true, cssProperty: 'height', dependentOn: 'width' },
+    { selector: '.app-title span', percentage: 0.015, exponent: 1,  cssProperty: 'fontSize', dependentOn: 'height' },
+    { selector: '.app-form p', percentage: 0.01, exponent: 1,  cssProperty: 'fontSize', dependentOn: 'height' },
 ];
 
 // Run the function on page load with the configuration
